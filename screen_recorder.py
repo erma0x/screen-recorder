927b76c6823bace1aa96fa92719528d9971ec2c8
erma0x <35337869+erma0x@users.noreply.github.com>
2022-09-02 17:42:49 +0200

    Update screen_recorder.py

--- screen_recorder.py
+++ screen_recorder.py
@@ -1,26 +1,31 @@
-
 import numpy as np
 from PIL import ImageGrab
 import time
+import os
+import datetime
 
-# SAVE JUST A BOX
-#box = (1106,657,1166,679) #Upper left and lower right corners
-#ImageGrab.grab(box).save('box.png') 
-
-
+ #Upper left and lower right corners
+#box = (1106,657,1166,679)
 box = (0,0,1920,1080) 
-
 sleeping_time = 2 # in seconds
+images_taken = 0
 
-images_taken=1
-
-while True:
-           
-           ImageGrab.grab(box).save('images/screenshot {0}.png'.format(time.asctime())) # SAVE FULLSCREEN
-           
-           time.sleep( sleeping_time ) 
-           
-           print(images_taken)
-          
-           images_taken+=1
+LOGO = """                                                                       _            
+                                                                      | |            
+  ___  ____  ____ _____ _____ ____      ____ _____  ____ ___   ____ __| |_____  ____ 
+ /___)/ ___)/ ___) ___ | ___ |  _ \    / ___) ___ |/ ___) _ \ / ___) _  | ___ |/ ___)
+|___ ( (___| |   | ____| ____| | | |  | |   | ____( (__| |_| | |  ( (_| | ____| |    
+(___/ \____)_|   |_____)_____)_| |_|  |_|   |_____)\____)___/|_|   \____|_____)_|    
+                                                                                     
+                                                                                    
+"""
+if __name__ == '__main__':
+    while True:
+        ImageGrab.grab(box).save('images/screenshot {0}.png'.format(time.asctime())) # SAVE FULLSCREEN
+        time.sleep( sleeping_time ) 
+        os.system('clear')
+        images_taken+=1
+        print(LOGO)
+        print('\n\t'+str(time.asctime()))
+        print(f'\n\tnumber images taken: [{images_taken}]')
                       
